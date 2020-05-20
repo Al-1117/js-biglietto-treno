b615c98d93c618becfa93eff49a45d841dd56c47
Alioune <alioune1117@gmail.com>
2020-05-20 16:00:19 +0200

    caricato i file dell'esercizio. manca ancora  la parte html

---
+++ js/script.js
@@ -0,0 +1,51 @@
+
+// Chiedo all'utente le informazioni necessarie.
+var age = parseInt( prompt("Quanti anni hai?"));
+var km = parseInt(prompt("Quanti chilometri percorrerai?"));
+console.log(age);
+console.log(km);
+
+// chilometri percorsi
+var ticketPrice = km * 0.21;
+console.log(ticketPrice);
+
+// sconto in base all'eta
+
+var discountYoung = ticketPrice * 20 / 100;
+
+var discountOlder = ticketPrice * 40 / 100;
+
+console.log (discountYoung)
+
+console.log(discountOlder);
+
+
+// Elaborazione prezzo finale
+
+var finalPrice
+
+if (age <= 17) {
+
+  finalPrice = ticketPrice - discountYoung;
+
+}
+
+else if (age >= 65) {
+
+  finalPrice = ticketPrice - discountOlder;
+
+}
+
+else ((age >= 18) || (age <= 64)) {
+
+  finalPrice = ticketPrice - 0;
+}
+
+console.log(finalPrice);
+
+
+
+
+
+
+// sconti
